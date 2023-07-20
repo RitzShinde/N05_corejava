157516affde268bd241a82747b22708de4e22147
RitzShinde <www.ritesh.shinde.1084@gmail.com>
2023-07-20 12:04:53 +0530

    First Commit

---
+++ dayone/src/met/tnsif/intro/First.java
@@ -0,0 +1,23 @@
+package met.tnsif.intro;
+import java.util.*;
+
+public class First {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		Scanner sc = new Scanner(System.in);
+		
+		//int num = sc.nextInt();
+		//System.out.println("the given number is:" + num);
+		
+//		float n = sc.nextFloat();
+//		System.out.println("the number is:"+ n);
+		
+		char chr = sc.next().charAt(1);
+		System.out.println("the string is:"+ chr);
+		
+
+	}
+
+}
